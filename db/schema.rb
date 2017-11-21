7944449e7d50b5865776afcc296657d4c3dd0144
Tenzin10 <tenzin.dorjee.1534@gmail.com>
2017-11-21 22:28:13 +0600

    clean up

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20171119182414) do
 
-  create_table "bookings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.string   "from"
-    t.string   "to"
-    t.integer  "days"
-  end
+ 
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
